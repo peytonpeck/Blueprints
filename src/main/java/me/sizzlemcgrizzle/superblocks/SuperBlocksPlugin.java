3a9f93eab46e201950fdc655611d81e8789ea101
SizzleMcGrizzle <57070735+SizzleMcGrizzle@users.noreply.github.com>
2020-03-17 12:28:31 -0500

    Made new plugin

---
+++ src/main/java/me/sizzlemcgrizzle/superblocks/SuperBlocksPlugin.java
@@ -0,0 +1,11 @@
+package me.sizzlemcgrizzle.superblocks;
+
+import org.mineacademy.fo.plugin.SimplePlugin;
+
+public class SuperBlocksPlugin extends SimplePlugin {
+	@Override
+	public void onPluginStart() {
+	}
+
+
+}
---
+++ src/main/java/me/sizzlemcgrizzle/superblocks/bell/SuperBell.java
@@ -0,0 +1,5 @@
+package me.sizzlemcgrizzle.superblocks.bell;
+
+public class SuperBell {
+	
+}
